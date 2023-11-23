8bd64633dfd456cba22ac276d030b8ccad737c04
Jia Yang <20104736@mail.wit.ie>
2023-11-23 12:45:37 +0000

    Added Filtering Test.

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,12 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+export const filterByTitle = (movieList, string) =>
+    movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
+
+export const filterByGenre = (movieList, genreId) =>
+    movieList.filter((m) => m.genre_ids.includes(genreId));
+
+export const filterByGenreAndTitle = (movieList, genreId, string) =>
+    movieList.filter((m) => (m.genre_ids.includes(genreId) && m.title.toLowerCase().search(string) !== -1));
